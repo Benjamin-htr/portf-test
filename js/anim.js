dc3a39262f91867536e3c5d03b425d0eaf85755f
Benjamin Hautier <benjamin.hautier@etudiant.univ-reims.fr>
2021-04-13 19:41:51 +0200

    link

--- js/anim.js
+++ js/anim.js
@@ -1,5 +1,16 @@
-console.log('barba');
 barba.init({
+    requestError: (trigger, action, url, response) => {
+      // go to a custom 404 page if the user click on a link that return a 404 response status
+      if (response.status && response.status === 404) {
+        console.log('error')
+      }
+
+      // prevent Barba from redirecting the user to the requested URL
+      // this is equivalent to e.preventDefault() in this context
+      return false;
+    },
+    /* debug: true,
+    logLevel: 'error', */
     transitions: [{
     name: 'opacity-transition',
     leave(data) {
@@ -10,8 +21,33 @@ barba.init({
     enter(data) {
       return gsap.from(data.next.container, {
         opacity: 0
+        
       });
     }
-  }]
+  }],
+  views: [
+    {
+        namespace: 'home',
+        afterEnter(data) {
+            console.log("sz-home");
+            timer('.fr');
+        }
+    },
+    {
+        namespace: 'home-en',
+        afterEnter(data) {
+            console.log("sz-home-en");
+            timer('.en');
+        }
+    }
+]
 });
+/* barba.hooks.afterEnter((data) => {
+    console.log("hooks", data)
+}); */
 console.log('barba');
+
+
+
+
+
